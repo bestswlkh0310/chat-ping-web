afa97e12204933364fc16d6314172cd75ba51387
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-04-21 20:22:25 +0900

    add api/v1

--- src/repository/http/Http.js
+++ src/repository/http/Http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const chatPingAxios = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'http://localhost:3001/api/v1',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',

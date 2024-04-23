a9795941b1c5992b75eb18407a6b8a7f32986c0d
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-04-23 13:58:31 +0900

    fix : font

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -4,7 +4,7 @@ export const GlobalStyle = createGlobalStyle`
     * {
         margin: 0;
         padding: 0;
-        font-family: 'Roboto';
+        font-family: 'Roboto Light',sans-serif;
     }
     :root {
         --primary: radial-gradient(circle at 50% 50%, rgba(255, 3, 133, 1) 0%, rgba(250, 188, 14, 1) 100%);
